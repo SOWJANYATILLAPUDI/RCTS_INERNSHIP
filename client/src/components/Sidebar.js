9e399c7f2c6c1c14bf92136315b08cb9aac2134b
SOWJANYATILLAPUDI <104555675+SOWJANYATILLAPUDI@users.noreply.github.com>
2023-11-23 10:09:51 +0530

    Updated Sidebar.js

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import '../styles/Sidebar.css';
+import { PiCardsFill } from "react-icons/pi";
+import { IoPieChartSharp } from "react-icons/io5";
+import { FaTableList } from "react-icons/fa6";
+import { FaFileWaveform } from "react-icons/fa6";
 const Sidebar = ({ isOpen, closeSidebar }) => {
   const sidebarClass = isOpen ? 'sidebar open' : 'sidebar';
 
@@ -9,20 +13,17 @@ const Sidebar = ({ isOpen, closeSidebar }) => {
         &times;
       </button>
       <ul>
-        {/* <li>
-          <a href="#content">Content</a>
-        </li> */}
         <li>
-          <a href="#cards">Cards</a>
+          <a href="#cards"><PiCardsFill/>  <b>Cards</b></a>
         </li>
         <li>
-          <a href="#piecharts">Charts</a>
+          <a href="#piecharts"><IoPieChartSharp/>  <b>Charts</b></a>
         </li>
         <li>
-          <a href="#table">Table</a>
+          <a href="#table"><FaTableList/>  <b>Table</b></a>
         </li>
         <li>
-          <a href="#form">Form</a>
+          <a href="#form"><FaFileWaveform/>  <b>Form</b></a>
         </li>
       </ul>
     </div>
